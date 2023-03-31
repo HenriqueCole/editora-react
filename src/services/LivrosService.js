f5b71bd1ec1ec14b190370672edc64067d44b416
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2023-03-30 21:44:07 -0300

    fix: remove https and the .env

--- src/services/LivrosService.js
+++ src/services/LivrosService.js
@@ -1,39 +1,38 @@
 import axios from "axios";
-const url = "https://localhost:8443/editora-livros-api/livro";
+const url = "http://localhost:8443/editora-livros-api/livro";
 
 export class LivrosService {
+  postLivro(livro) {
+    return axios.post(url, livro);
+  }
 
-    postLivro(livro) {
-        return axios.post(url, livro);
-    }
+  getLivros() {
+    const config = {
+      withCredentials: true,
+    };
+    return axios
+      .get(url, config)
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    getLivros() {
-        const config = {
-            withCredentials: true,
-        };
-        return axios.get(url,config)
-            .then(response => {
-                return(response);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
-    
-    getStatus() {
-        return axios.get(url + "/status");
-    }
+  getStatus() {
+    return axios.get(url + "/status");
+  }
 
-    getLivro(isbn) {
-        return axios.get(url + "/isbn/" + isbn);
-    }
+  getLivro(isbn) {
+    return axios.get(url + "/isbn/" + isbn);
+  }
 
-    deleteLivro(isbn) {
-        return axios.delete(url + "/" + isbn);
-    }
+  deleteLivro(isbn) {
+    return axios.delete(url + "/" + isbn);
+  }
 
-    putLivro(isbn , livro) {
-        return axios.put(url + "/" + isbn, livro);
-    }
-
+  putLivro(isbn, livro) {
+    return axios.put(url + "/" + isbn, livro);
+  }
 }
--- src/services/PessoasService.js
+++ src/services/PessoasService.js
@@ -1,33 +1,31 @@
 import axios from "axios";
 
-const url = "https://localhost:8443/editora-livros-api/pessoa";
+const url = "http://localhost:8443/editora-livros-api/pessoa";
 
 class PessoasService {
+  postAutor(pessoa) {
+    return axios.post(url, pessoa);
+  }
 
-    postAutor(pessoa) {
-        return axios.post(url, pessoa);
-    }
+  postRevisor(pessoa) {
+    return axios.post(url + "/revisor", pessoa);
+  }
 
-    postRevisor(pessoa) {
-        return axios.post(url + "/revisor", pessoa);
-    }
+  getGeneros() {
+    return axios.get(url + "/generos");
+  }
 
-    getGeneros() {
-        return axios.get(url + "/generos");
-    }
+  getPessoa(cpf) {
+    return axios.get(url + "/" + cpf);
+  }
 
-    getPessoa(cpf) {
-        return axios.get(url + "/" + cpf);
-    }
+  deletePessoa(cpf) {
+    return axios.delete(url + "/" + cpf);
+  }
 
-    deletePessoa(cpf) {
-        return axios.delete(url + "/" + cpf);
-    }
-
-    putPessoa(cpf , pessoa) {
-        return axios.put(url + "/" + cpf, pessoa);
-    }
-
+  putPessoa(cpf, pessoa) {
+    return axios.put(url + "/" + cpf, pessoa);
+  }
 }
 
 export default PessoasService;
